c5f2ca470e3835c161f8ac6ae790e53b6fe5efed
anoopk <anoop_kr@yahoo.com>
2019-03-12 22:20:46 +0530

    SDK initialized. Some funtions listed

--- client.js
+++ client.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 var event0 = {"metadata": {"organization": "Hyundai", "product": "i20"}};
 var event1 = {"metadata": {"organization": "Hyundai", "product": "i10"}};
 var event2 = {"metadata": {"organization": "maruti", "product": "wagonr"}};
-sdk.handler(event0, {}).then(function(status, error){
-	console.log("Gotcha ", status);
+sdk.getDocumentCount(event1, {}).then(function(status, error){
+	console.log("Gotcha");		
 });
+
--- index.js
+++ index.js
@@ -3,18 +3,44 @@
 var config = require('./config.json').mongodb
 var MongoClient = require('mongodb').MongoClient
 
-exports.handler = async (event, context, callback) => {
+//system.organizationCount
+//system.organizations
+//system.aspects
+
+//wiki.organizations
+//wiki.products
+
+//organization.sentimentSnapshot = async (org) => total reviews/positive/negative
+//organization.productCount
+//organization.products
+//organization.mostPositive(howmany, whattimeperiod) => URLList
+
+//product.sentimentSnapshot() = async (org) => total reviews/positive/neutral/negative
+//product.aspectSnapshot => total reviews/positive/negative per aspect
+
+
+//aspect.getSentimentData(product, aspect)
+
+exports.getDocumentCount = async (event, context, callback) => {
 	var filter = event;
 	context.callbackWaitsForEmptyEventLoop = false;
 	let client = await MongoClient.connect(config.url, { useNewUrlParser: true });	
-	let db = client.db(config.db);				
+	let db = client.db(config.db);		
+	var sentimentStats = {"positive": 0, "negative": 0, "reviews": 0};
 	try{
-		var coll = db.collection('abs');
-		let result = await coll.countDocuments({"metadata.organization": filter.metadata.organization, "metadata.product": filter.metadata.product});
-		return result;		
+		var coll = db.collection('sentiment');
+		let results = await coll.find({"metadata.organization": filter.metadata.organization}).forEach(function(doc){
+			console.log(doc.result);
+		});
+		client.close();
+		//sentimentStats.reviews++;
+		//let r = result.result;
+		//console.log(r);
+		//});		
+		return sentimentStats;		
 	}	
 	finally{
-		client.close();
+		
 	}
 }
 
