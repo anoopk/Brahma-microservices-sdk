fe068125d29e80eea7880928a36b5dc1bce5cbc5
anoopk <anoop_kr@yahoo.com>
2019-03-15 22:49:00 +0530

    Uptil products

--- client.js
+++ client.js
@@ -13,20 +13,20 @@ autoworld.startup(config).then(function(aw){
 		console.log("Total unique reviews ", count);		
 	});
 
-	aw.organisations.count(event1, config).then(function(count, error){
+	const org = 'Maruti';
+	aw.organisations.count().then(function(count, error){
 		console.log("Organizations  ", count);		
 	});
-
-	const org = "Maruti";	
+		
 	aw.products(org).then(function(count, error){
 		console.log("Products ", count);		
 	});
 
-	//aw.products.count(event1, config).then(function(count, error){
-	//	//console.log("Products ", count);		
-	//});
+	aw.products.count(event1, config).then(function(count, error){
+		console.log("Products ", count);		
+	});
 	
-	aw.organisations(event1, config).then(async function(orgs, error){
+	aw.organisations("Maruti").then(async function(orgs, error){
 		console.log("Organizations ", orgs);
 		orgs.map(org => aw.organisations.sentiments(org).then(function(sentiments){
 			console.log(sentiments);
@@ -36,7 +36,7 @@ autoworld.startup(config).then(function(aw){
 	
 	//aw.organisations.sentiments(org).then(function(ss, error){
 	//	console.log("Sentiment snapshot for ", org, ss);
-	//	aw.shutdown();		
+		//aw.shutdown();		
 	//
 });
 
